ef9ca70dbb6282ec70907c4cf2940aefe7d91e3e
miketreacy <michael.treacy@live.com>
2019-11-06 08:42:53 -0800

    Initial commit for file IO microservice

---
+++ README.md
@@ -0,0 +1,6 @@
+# MOTIVIC CONVERTOR
+Future site of file I/O microservice to convert files from
+1. MOTIF => MIDI
+2. MIDI => WAV
+3. WAV => MP3
+4. MOTIF <==> JSON
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+// Future site of file I/O microservice to convert files from
+// 1. MOTIF => MIDI
+// 2. MIDI => WAV
+// 3. WAV => MP3
+// 4. MOTIF <==> JSON
+func main() {
+	// leverage these packages for conversion:
+	// "github.com/go-audio/midi"
+	// "github.com/go-audio/wav"
+
+}
